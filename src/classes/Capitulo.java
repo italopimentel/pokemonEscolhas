d9163eb589f48ea7f3e7b5897cc0020df894120a
OfelipeO <garcez581@gmail.com>
2021-12-07 21:25:54 -0300

    Adicionei o script capítulo

---
+++ src/classes/Capitulo.java
@@ -0,0 +1,65 @@
+package classes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Capitulo {
+	
+	private String contexto;
+	private List<Capitulo> proximosCapitulos= new ArrayList<>();
+	private String resumo;
+	private int energia;
+	
+	
+	public Capitulo(String contexto, String resumo, int energia) {
+		super();
+		this.contexto = contexto;
+		this.resumo = resumo;
+		this.energia = energia;
+	}
+	
+	public String getContexto() {
+		return contexto;
+	}
+
+	public void setContexto(String contexto) {
+		this.contexto = contexto;
+	}
+
+	public List<Capitulo> getProximosCapitulos() {
+		return proximosCapitulos;
+	}
+
+	public void setProximosCapitulos(List<Capitulo> proximosCapitulos) {
+		this.proximosCapitulos = proximosCapitulos;
+	}
+
+	public String getResumo() {
+		return resumo;
+	}
+
+	public void setResumo(String resumo) {
+		this.resumo = resumo;
+	}
+
+	public int getEnergia() {
+		return energia;
+	}
+
+	public void setEnergia(int energia) {
+		this.energia = energia;
+	}
+
+	public void adicionarCapitulo(Capitulo capitulo)
+	{      // Adicionar condição de não por mais que dois
+		proximosCapitulos.add(capitulo);
+	}
+
+	@Override
+	public String toString()
+	{
+		return resumo;
+	}
+	
+
+}
